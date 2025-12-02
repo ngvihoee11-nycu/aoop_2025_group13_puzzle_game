e673181aa46868d2455a569e9c745994467a6aa4
jeffjunj113 <jeffjunj113@gmail.com>
2025-12-02 23:26:05 +0800

    Add GameManager,FPS limit(60) and counter

--- Assets/Scripts/Managers/GamaManager.cs
+++ Assets/Scripts/Managers/GamaManager.cs
@@ -1,10 +1,14 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+int targetFPS = 60 
+
 public class GamaManager : Singleton<GamaManager>
 {
     private void Start()
     {
+        QualitySettings.vSyncCount = 0; // Disable VSync to use targetFrameRate
+        Application.targetFrameRate = targetFPS;
         SceneManager.LoadScene("MainMenu", LoadSceneMode.Additive);
     }
 }
