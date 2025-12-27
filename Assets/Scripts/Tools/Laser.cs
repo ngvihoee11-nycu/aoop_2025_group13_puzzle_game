3d10922eaa08106511d10e7955ac1e5db6a9e18a
ngviho11ee-nycu <ngviho.ee11@nycu.edu.tw>
2025-12-28 00:41:03 +0800

    feat: Update portal prefab, remove unused properties, and add mirror tag

--- Assets/Scripts/Tools/Laser.cs
+++ Assets/Scripts/Tools/Laser.cs
@@ -6,9 +6,6 @@ public class Laser : MonoBehaviour
 {
     public Vector3 startPosition;
     public Collider exitingCollider;
-    private bool recentlyTeleported = false;
-    private float teleportCooldown = 0.15f;
-    private Coroutine teleportCoroutine;
 
     void OnTriggerEnter(Collider other)
     {
@@ -20,19 +17,4 @@ public class Laser : MonoBehaviour
         GetComponentInParent<LaserEmitter>().EmitLaser();
     }
 
-    private IEnumerator ResetTeleportFlag(float duration)
-    {
-        yield return new WaitForSeconds(duration);
-        recentlyTeleported = false;
-        teleportCoroutine = null;
-    }
-
-    // Public method to begin a teleport/reflection cooldown (called by Mirror and portal teleport logic)
-    public void BeginTeleportCooldown(float duration)
-    {
-        recentlyTeleported = true;
-        if (teleportCoroutine != null)
-            StopCoroutine(teleportCoroutine);
-        teleportCoroutine = StartCoroutine(ResetTeleportFlag(duration));
-    }
 }
